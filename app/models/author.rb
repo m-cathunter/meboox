2d7cb7116e1f9bcdd7c70bbd936cb46d89ac1ea4
Magdalena Kotołowska <m.kotolowska@gmail.com>
2022-07-13 07:52:30 +0200

    Provide the fullname of the authors

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,6 +3,10 @@ class Author < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def full_name
+    "#{last_name} #{first_name}"
+  end
 end
 
 # == Schema Information
--- app/serializers/author_serializer.rb
+++ app/serializers/author_serializer.rb
@@ -2,4 +2,5 @@ class AuthorSerializer < ActiveModel::Serializer
   attribute :id
   attribute :first_name
   attribute :last_name
+  attribute :full_name
 end
