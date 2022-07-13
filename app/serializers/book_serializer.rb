f79593031eb982662795f2fa5eeba53a740e2d9d
Magdalena Kotołowska <m.kotolowska@gmail.com>
2022-07-13 08:44:26 +0200

    Improve the book serializer

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -4,4 +4,10 @@ class BookSerializer < ActiveModel::Serializer
   attribute :id
   attribute :title
   attribute :publication_date
+  attribute :type
+  attribute :duration, if: :audiobook?
+
+  def audiobook?
+    object.is_a? AudioBook
+  end
 end
